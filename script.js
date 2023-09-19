35ac52f3a8974b93bda67ba4aa2495393138e57f
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-09-19 17:04:59 +0200

    err img

--- script.js
+++ script.js
@@ -9,33 +9,27 @@ const searchUser=document.getElementById('searchField').value
         .then(displayUserData)
         .catch(error => {
             console.log(error.message)
+            alert('Nessuno dato trovato')
         })
 }
 
 function displayUserData(datiAlbum) {
     console.log(datiAlbum.data[0]) // per guardare la struttura dati resituita
-
+    console.log(datiAlbum.data.length)
     //nome dell'artista
     console.log(datiAlbum.data[0].artist.name)
     //picture_small
     console.log(datiAlbum.data[0].artist.picture)
     //titolo album
     console.log(datiAlbum.data[0].album.title)
-    //carico nome artista su dom
+    //carico nome e cover artista su dom
     let titolo=document.getElementById('titolotrovato')
     let cover_album=document.getElementById('covertrovata')
     titolo.innerHTML=datiAlbum.data[0].artist.name
     cover_album.src=datiAlbum.data[0].artist.picture
-    console.log(titolo)
+
     let card=document.getElementById('card_all_element')
-    card.classList.add('d-flex')
-    // let vis_ricerca=document.querySelector('#found div')
-    // vis_ricerca.appendChild(titolo)
-    // vis_ricerca.appendChild(cover_album)
-
-    // document.querySelector("#full-name").innerText = `${person.name.title} ${person.name.first} ${person.name.last}`
-    // document.querySelector("#avatar").src = person.picture.large
-    // document.querySelector("#cell").innerText = person.cell
-
+    card.classList.add('d-flex','h-100')
+    
     // document.querySelector("#address").innerText = `${person.location.street.name}, ${person.location.street.number}, ${person.location.state}, ${person.location.country}`
 }
